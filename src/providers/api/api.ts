c9d5265a7e17c586e030312b9a9230e1ae6685f7
SidMorad <SidMorad@users.noreply.github.com>
2018-02-17 15:19:30 +0330

    Changes(by jhipster-ionic) after detecting Mars/gateway is using OAuth2-oidc authentication.

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
  */
 @Injectable()
 export class Api {
-  public static API_URL: string = 'http://localhost:8080/api';
+  public static API_URL: string = 'https://mars.webebook.org/api';
 
   constructor(public http: HttpClient) {
   }
