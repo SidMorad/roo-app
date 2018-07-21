0d6ae55e59cc9a4c3780c8271a48c25d5347ac9e
SidMorad <SidMorad@users.noreply.github.com>
2018-07-21 13:51:29 +0430

    Generating more picture question type when number of pictures is more than 4.

--- src/providers/question-generator/question-generator.ts
+++ src/providers/question-generator/question-generator.ts
@@ -23,91 +23,67 @@ export class QuestionGenerator {
   }
 
   private generateBeginner(pictures, subjects, data) {
-    let res = [];
-    const maxIndex = subjects.length - 1;
-    const maxPicIndex = pictures.length - 1;
-    if (maxPicIndex >= 1) {
-      res.push(this.generateTwoPicture(data[pictures[0]], data[pictures[1]], 10, pictures[0], pictures[1]));
-    }
-    if (maxPicIndex >= 3) {
-      res.push(this.generateTwoPicture(data[pictures[2]], data[pictures[3]], 20, pictures[2], pictures[3]));
-    } else if (maxPicIndex >= 1) {
-      res.push(this.generateTwoPicture(data[pictures[1]], data[pictures[0]], 20, pictures[1], pictures[0]));
-    }
-    if (maxPicIndex >= 3) {
-      res.push(this.generateOneCheck(pictures[this.randomBetween(0,1)], 30, [pictures[0], pictures[1], pictures[2], pictures[3]]));
-    }
-    if (maxPicIndex >= 3) {
-      res.push(this.generateFourPicture(data[pictures[0]], data[pictures[1]],
-                                   data[pictures[2]], data[pictures[3]], 40,
-                                   pictures[0], pictures[1], pictures[2], pictures[3]));
-    }
-    if (maxPicIndex >= 3) {
-      res.push(this.generateFourPicture(data[pictures[0]], data[pictures[1]],
-                                   data[pictures[2]], data[pictures[3]], 60,
-                                   pictures[0], pictures[1], pictures[2], pictures[3]));
-    }
-    if (maxPicIndex >= 3) {
-      res.push(this.generateOneCheck(pictures[this.randomBetween(2,3)], 50, [pictures[0], pictures[1], pictures[2], pictures[3]]));
+    let res = []; const maxIndex = subjects.length - 1; const maxPicIndex = pictures.length - 1;
+    for (let i = 0; i < maxPicIndex; i = i + 4) {
+      if (maxPicIndex >= i + 1)
+        res.push(this.generateTwoPicture(data[pictures[i]], data[pictures[i+1]], 10+i*i, pictures[i], pictures[i+1]));
+      if (maxPicIndex >= i + 3)
+        res.push(this.generateTwoPicture(data[pictures[i+2]], data[pictures[i+3]], 20+i*i, pictures[i+2], pictures[i+3]));
+      else if (maxPicIndex >= i + 1)
+        res.push(this.generateTwoPicture(data[pictures[i+1]], data[pictures[i]], 30+i*i, pictures[i+1], pictures[i]));
+      if (maxPicIndex >= i + 3)
+        res.push(this.generateOneCheck(pictures[this.randomBetween(i,i+1)], 40+i*i, [pictures[i], pictures[i+1], pictures[i+2], pictures[i+3]]));
+      if (maxPicIndex >= i + 3)
+        res.push(this.generateFourPicture(data[pictures[i]], data[pictures[i+1]], data[pictures[i+2]], data[pictures[i+3]], 50+i*i,
+                                               pictures[i],       pictures[i+1],       pictures[i+2],       pictures[i+3]));
+      if (maxPicIndex >= i + 3)
+        res.push(this.generateFourPicture(data[pictures[i]], data[pictures[i+1]], data[pictures[i+2]], data[pictures[i+3]], 60+i*i,
+                                               pictures[i],       pictures[i+1],       pictures[i+2],       pictures[i+3]));
+      if (maxPicIndex >= i + 3)
+        res.push(this.generateOneCheck(pictures[this.randomBetween(i+2,i+3)], 70+i*i, [pictures[i], pictures[i+1], pictures[i+2], pictures[i+3]]));
     }
     for (let i = 0; i < 13; i = i + 6) {
-      if (maxIndex >= i+5) {
+      if (maxIndex >= i+5)
         res.push(this.generateSpeaking(50+i*20, subjects[this.randomBetween(i, i+5)]));
-      }
-      if (maxIndex >= i+3) {
+      if (maxIndex >= i+3)
         res.push(this.generateOneCheck(subjects[i], 60+i*20, [subjects[i], subjects[i+1], subjects[i+2], subjects[i+3]]));
-      }
-      if (maxIndex >= i+1) {
+      if (maxIndex >= i+1)
         res.push(this.generateMultiSelect(subjects[i+1], 70+i*20, [subjects[this.randomLessThan(i+1)]], this.trueOrFalse()));
-      }
-      if (maxIndex >= i+2) {
+      if (maxIndex >= i+2)
         res.push(this.generateMultiSelect(subjects[i+2], 90+i*20, [subjects[this.randomLessThan(i+2)]], this.trueOrFalse()));
-      }
-      if (maxIndex >= i+3) {
+      if (maxIndex >= i+3)
         res.push(this.generateOneCheck(subjects[i+3], 80+i*20, [subjects[i], subjects[i+1], subjects[i+2], subjects[i+3]]));
-      }
-      if (maxIndex >= i+4) {
+      if (maxIndex >= i+4)
         res.push(this.generateMultiSelect(subjects[i+4], 100+i*20, [subjects[this.randomLessThan(i+4)]], this.trueOrFalse()));
-      }
-      if (maxIndex >= i+5) {
+      if (maxIndex >= i+5)
         res.push(this.generateMultiSelect(subjects[i+5], 50+i*20, [subjects[this.randomLessThan(i+5)]], this.trueOrFalse()));
-      }
     }
-    if (maxPicIndex >= 3) {
+    if (maxPicIndex >= 3)
       res.push(this.generateWriting(1099, pictures[this.randomBetween(0,3)], this.trueOrFalse()));
-    } else if (maxPicIndex >= 1) {
+    else if (maxPicIndex >= 1)
       res.push(this.generateWriting(1099, pictures[this.randomBetween(0,1)], this.trueOrFalse()));
-    }
     return res;
   }
 
   private generateIntermediate(pictures, subjects, data) {
-    let res = [];
-    const maxIndex = subjects.length - 1;
-    const maxPicIndex = pictures.length - 1;
-    if (maxPicIndex >= 1) {
-      res.push(this.generateTwoPicture(data[pictures[0]], data[pictures[1]], 10, pictures[0], pictures[1]));
-    }
-    if (maxPicIndex >= 3) {
-      res.push(this.generateTwoPicture(data[pictures[2]], data[pictures[3]], 20, pictures[2], pictures[3]));
-    } else if (maxPicIndex >= 1) {
-      res.push(this.generateTwoPicture(data[pictures[1]], data[pictures[0]], 20, pictures[1], pictures[0]));
-    }
-    if (maxPicIndex >= 1) {
-      res.push(this.generateWriting(22, pictures[this.randomBetween(0,1)], this.trueOrFalse()));
-    }
-    if (maxPicIndex >= 3) {
-      res.push(this.generateFourPicture(data[pictures[0]], data[pictures[1]],
-                                   data[pictures[2]], data[pictures[3]], 40,
-                                   pictures[0], pictures[1], pictures[2], pictures[3]));
-    }
-    if (maxPicIndex >= 3) {
-      res.push(this.generateFourPicture(data[pictures[0]], data[pictures[1]],
-                                   data[pictures[2]], data[pictures[3]], 44,
-                                   pictures[0], pictures[1], pictures[2], pictures[3]));
-    }
-    if (maxPicIndex >= 3) {
-      res.push(this.generateWriting(48, pictures[this.randomBetween(2,3)], this.trueOrFalse()));
+    let res = []; const maxIndex = subjects.length - 1;  const maxPicIndex = pictures.length - 1;
+    for (let i = 0; i < maxPicIndex; i = i + 4) {
+      if (maxPicIndex >= i + 1)
+        res.push(this.generateTwoPicture(data[pictures[i]], data[pictures[i+1]], 10+i*i, pictures[i], pictures[i+1]));
+      if (maxPicIndex >= i + 3)
+        res.push(this.generateTwoPicture(data[pictures[i+2]], data[pictures[i+3]], 20+i*i, pictures[i+2], pictures[i+3]));
+      else if (maxPicIndex >= i + 1)
+        res.push(this.generateTwoPicture(data[pictures[i+1]], data[pictures[i]], 30+i*i, pictures[i+1], pictures[i]));
+      if (maxPicIndex >= i + 1)
+        res.push(this.generateWriting(40+i*i, pictures[this.randomBetween(i,i+1)], this.trueOrFalse()));
+      if (maxPicIndex >= i + 3)
+        res.push(this.generateFourPicture(data[pictures[i]], data[pictures[i+1]], data[pictures[i+2]], data[pictures[i+3]], 50+i*i,
+                                               pictures[i],       pictures[i+1],       pictures[i+2],       pictures[i+3]));
+      if (maxPicIndex >= i + 3)
+        res.push(this.generateFourPicture(data[pictures[i]], data[pictures[i+1]], data[pictures[i+2]], data[pictures[i+3]], 60+i*i,
+                                               pictures[i],       pictures[i+1],       pictures[i+2],       pictures[i+3]));
+      if (maxPicIndex >= i + 3)
+        res.push(this.generateWriting(70+i*i, pictures[this.randomBetween(i+2,i+3)], this.trueOrFalse()));
     }
     for (let i = 0; i < 11; i = i + 5) {
       if (maxIndex >= i+4) {
@@ -141,20 +117,14 @@ export class QuestionGenerator {
 
   private generateAdvanced(pictures, subjects, data) {
     let res = [];
-    const maxPicIndex = pictures.length - 1;
-    if (maxPicIndex >= 1) {
-      res.push(this.generateWriting(10, pictures[this.randomBetween(0,1)], this.trueOrFalse()));
-    }
-    if (maxPicIndex >= 3) {
-      res.push(this.generateWriting(20, pictures[this.randomBetween(2,3)], this.trueOrFalse()));
-    }
-    if (maxPicIndex >= 3) {
-      res.push(this.generateFourPicture(data[pictures[0]], data[pictures[1]],
-                                   data[pictures[2]], data[pictures[3]], 30,
-                                   pictures[0], pictures[1], pictures[2], pictures[3]));
-    }
-
     const randomReverseWritingCounter = this.randomBetween(2,5);
+    for (let i = 0; i < pictures.length; i++) {
+      if (i % randomReverseWritingCounter === 0) {
+        res.push(this.generateWriting(10+i, pictures[i], true));
+      } else {
+        res.push(this.generateWriting(10+i, pictures[i]));
+      }
+    }
     for (let i = 0; i < subjects.length; i++) {
       if (i % randomReverseWritingCounter === 0) {
         res.push(this.generateWriting(40+i, subjects[i], true));
